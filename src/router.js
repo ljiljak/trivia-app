053f8699847ebfa3e49a6a98f1c034d8f78558b6
ljiljak <ljiljakaleksandar@gmail.com>
2018-05-18 18:47:09 +0200

    Z1

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import TriviaApp from './components/TriviaApp.vue' 
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+	routes: [
+		{ path: '/', components: TriviaApp }
+	]
+})
